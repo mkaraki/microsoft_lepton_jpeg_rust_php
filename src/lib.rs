ae1df198a79843a62b810f5f6627bf0ea4c98c32
mkaraki <26180919+mkaraki@users.noreply.github.com>
2024-07-25 16:15:29 +0900

    Remove usize cast

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,11 @@ use lepton_jpeg::enabled_features::EnabledFeatures;
 #[php_function]
 pub fn convert_lepton_to_jpeg(lepton: Binary<u8>) -> Option<Binary<u8>> {
     let input_lepton: Vec<u8> = Vec::from(lepton);
-    let input_size: u64 = input_lepton.len() as u64;
+    let input_size: usize = input_lepton.len();
 
     let mut reader = std::io::Cursor::new(input_lepton);
 
-    let output_buffer_size: u64 = input_size * 3;
-    let mut output_buffer: Vec<u8> = Vec::with_capacity(output_buffer_size as usize);
+    let mut output_buffer: Vec<u8> = Vec::with_capacity(input_size * 3);
 
     match decode_lepton(
         &mut reader,
@@ -29,11 +28,10 @@ pub fn convert_lepton_to_jpeg(lepton: Binary<u8>) -> Option<Binary<u8>> {
 #[php_function]
 pub fn convert_jpeg_to_lepton(lepton: Binary<u8>) -> Option<Binary<u8>> {
     let input_jpeg: Vec<u8> = Vec::from(lepton);
-    let input_size: u64 = input_jpeg.len() as u64;
+    let input_size: usize = input_jpeg.len();
     let mut reader = std::io::Cursor::new(input_jpeg);
 
-    let output_buffer_size: u64 = input_size * 2;
-    let mut output_buffer: Vec<u8> = Vec::with_capacity(output_buffer_size as usize);
+    let mut output_buffer: Vec<u8> = Vec::with_capacity(input_size * 2);
     let mut writer = std::io::Cursor::new(&mut output_buffer);
 
     let enabled_features = EnabledFeatures {
